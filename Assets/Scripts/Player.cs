aa5574da964a602d41bcb0e4e3bdbbb948ebd848
Celeste Fournier <gustavo.s.fournier@gmail.com>
2021-07-11 21:09:08 -0300

    Add Hugo movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,40 @@ public class Player : MonoBehaviour
 {
     public GameController gameController;
 
+    Animator anim;
     bool isJumping;
+    int position;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
         if (Input.GetButton("Submit") && !gameController.gameStarted && !isJumping)
         {
-            GetComponent<Animator>().SetTrigger("jumpRight");
+            anim.SetTrigger("jumpRight");
             isJumping = true;
+            position = 1;
+        }
+
+        if (Input.GetAxis("Horizontal") > 0 && !isJumping && position < 2)
+        {
+            anim.SetTrigger("jumpRight");
+            isJumping = true;
+            position++;
+        }
+        if (Input.GetAxis("Horizontal") < 0 && !isJumping && position > 0)
+        {
+            anim.SetTrigger("jumpLeft");
+            isJumping = true;
+            position--;
         }
     }
 
