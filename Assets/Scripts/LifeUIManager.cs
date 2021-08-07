436403c80f0c84ff4280a01aadeb39657ac7e6a3
Celeste Fournier <gustavo.s.fournier@gmail.com>
2021-08-07 16:57:23 -0300

    Add player lives on HUD

---
+++ Assets/Scripts/LifeUIManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeUIManager : MonoBehaviour
+{
+    public Image[] lives;
+    public Sprite fullLife, emptyLife;
+
+    void Start()
+    {   
+        if (PlayerPrefs.GetInt("Lives") == 2)
+        {
+            lives[0].sprite = fullLife;
+            lives[1].sprite = fullLife;
+        }
+
+        if (PlayerPrefs.GetInt("Lives") == 1)
+        {
+            lives[0].sprite = fullLife;
+            lives[1].sprite = emptyLife;
+        }
+    }
+}
