5571a71a95288f29eabfb8fb5ab3716f362cf65b
Celeste Fournier <gustavo.s.fournier@gmail.com>
2021-08-07 17:44:33 -0300

    Add score for player distance

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public GameController gameController;
+    public Transform player;
+
+    float playerStartPos;
+    int score;
+    Text scoreText;
+
+    void Start()
+    {
+        score = 0;
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+        gameController.onStart.AddListener(() => StartCoroutine("DistanceScore"));
+        playerStartPos = player.position.y;
+    }
+
+    IEnumerator DistanceScore()
+    {
+        while (gameController.gameStarted)
+        {
+            score = (int) ((player.position.y - playerStartPos) * 10) * 10;
+            scoreText.text = score.ToString();
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
